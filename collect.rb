6481e9afe48bff6f75f5f35d5990206d16411ef2
stuts <stu.j.franks@gmail.com>
2020-09-23 11:34:24 +0100

    Initial data collection script

---
+++ collect.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+
+# Vars
+dir = ARGV.first
+log = "collection/#{dir.gsub('/', '')}_#{Time.new.strftime("%Y-%m-%d")}"
+files = {}
+
+# Validation
+## Check dir exists
+## Maybe check dir permissions too?
+## Check log writable
+
+# Collect all stat info from directory
+puts "Scanning files in #{dir}, logging to #{log}"
+
+file_tree = Dir["#{dir}/**/*"]
+file_tree.each do |file|
+  files[file] = {}
+  files[file]['last_access'] = File.stat(file).atime
+  files[file]['size'] = File.stat(file).size
+
+  # Write out data (so can rerun on larged filesystems if needs be...)
+  File.write(log, files)
+end
+
